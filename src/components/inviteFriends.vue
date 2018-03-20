9d09798887fe7d4fceeccbc36ecdddc3b57003c7
周晓建 <xiaojian.zhou@creativearts.cn>
2018-03-20 16:26:47 +0800

    语法错

--- src/components/inviteFriends.vue
+++ src/components/inviteFriends.vue
@@ -101,7 +101,6 @@
       shareClicked() {
         if (!this.accountNumber) {
           popup(null, null, '请先登录！')
-          return
         } else {
           share(`您的好友${this.accountNumber}邀请您领万元大奖—可提现—`, 'http://106.75.216.201/android/#/share', '一款专业、快捷、方便的借款服务APP,为用户提供精准、优质的借款推荐服务。')
         }
